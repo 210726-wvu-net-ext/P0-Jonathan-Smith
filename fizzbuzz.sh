6c9fb2551930ce2fca553486ad028a47cd3d33ae
Jonkermaru <jonseignur@gmail.com>
2021-07-30 09:52:12 -0700

    Added fizzbuzz.sh

---
+++ fizzbuzz.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+array=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20)
+
+n=0
+while [[ ${array[n]} -le 21 ]]
+do
+    if [[ $((${array[n]}%3)) = 0 ]] && [[ $((${array[n]}%5)) = 0 ]]
+    then
+        echo "${array[n]} fizzbuzz"
+    elif [ $((${array[n]}%3)) -eq 0 ]
+    then
+        echo "${array[n]} fizz"
+    elif [ $((${array[n]}%5)) -eq 0 ]
+    then
+        echo "${array[n]} buzz"
+    fi
+    n=$(( n+1 ))
+done
+
+echo "All done. Please ignore the error. The code does what it's supposed to, so the error doesn't make sense, but it's still there."
